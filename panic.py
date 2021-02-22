6bd8d77055a0e84e4612bee9cb72187753697fe3
kpobrien96 <50643127+kpobrien96@users.noreply.github.com>
2021-02-22 17:44:42 -0500

    Update panic.py

--- panic.py
+++ panic.py
@@ -7,7 +7,7 @@ for i in range(4):
   plist.pop()
 plist.pop(0)
 plist.remove("'")
-plist.extend([plist.pop,plist.pop()])
+plist.extend([plist.pop(),plist.pop()])
 plist.insert(2, plist.pop(3))
 
 new_phrase = ''.join(plist)
